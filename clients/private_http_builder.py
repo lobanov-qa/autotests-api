185dce7b94001cd98729ae3fed60568be4e62b97
lobanov-qa <evgenii-lobanov-qa@yandex.ru>
2025-12-10 17:14:07 +0700

    Рефакторинг и подготовка к написанию API автотестов.Практика реализации фикстур

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,13 +1,14 @@
 from functools import lru_cache
 
 from httpx import Client
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
-class AuthenticationUserSchema(BaseModel, frozen=True):
+class AuthenticationUserSchema(BaseModel):
+    model_config = ConfigDict(frozen=True)
     email: str
     password: str
 
