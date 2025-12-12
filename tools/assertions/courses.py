2230e0777764ada96403d6bfe75e6d9f73eb04d5
lobanov-qa <evgenii-lobanov-qa@yandex.ru>
2025-12-12 10:14:57 +0700

    Практикуемся в написании API автотестов. Часть 2.Практика написания API автотестов для работы с курсами

--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -69,10 +69,10 @@ def assert_create_course_response(request: CreateCourseRequestSchema, response: 
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
 
-    assert_equal(request.title, response.course.title, "title")
-    assert_equal(request.max_score, response.course.max_score, "max_score")
-    assert_equal(request.min_score, response.course.min_score, "min_score")
-    assert_equal(request.description, response.course.description, "description")
-    assert_equal(request.estimated_time, response.course.estimated_time, "estimated_time")
-    assert_equal(request.preview_file_id, response.course.preview_file.id, "preview_file_id")
-    assert_equal(request.created_by_user_id, response.course.created_by_user.id, "created_by_user_id")
+    assert_equal(response.course.title, request.title, "title")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
+    assert_equal(response.course.min_score, request.min_score, "min_score")
+    assert_equal(response.course.description, request.description, "description")
+    assert_equal(response.course.estimated_time, request.estimated_time, "estimated_time")
+    assert_equal(response.course.preview_file.id, request.preview_file_id, "preview_file_id")
+    assert_equal(response.course.created_by_user.id, request.created_by_user_id,"created_by_user_id")
