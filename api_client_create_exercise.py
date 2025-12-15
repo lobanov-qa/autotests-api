4e084eabf0940a4b46aa19cbecb0f92c3e19b5e1
lobanov-qa <evgenii-lobanov-qa@yandex.ru>
2025-12-15 21:39:50 +0700

    Настройки автотестов с Pydantic.Практика работы с настройками

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -7,6 +7,7 @@ from clients.exercises.exercises_schema import CreateExerciseRequestSchema
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
+from config import settings
 
 public_users_client = get_public_users_client()
 
@@ -25,7 +26,7 @@ exercises_client = get_exercises_client(authentication_user)
 
 # Загружаем файл
 create_file_request = CreateFileRequestSchema(
-    upload_file="./testdata/files/image.png"
+    upload_file=settings.test_data.image_png_file
 )
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
