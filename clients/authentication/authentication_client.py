5eda41c996d552e6457ceb288513e9350d824072
lobanov-qa <evgenii-lobanov-qa@yandex.ru>
2025-12-16 15:29:08 +0700

    Улучшение работы с API клиентами с помощью Enum для URI

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -5,6 +5,8 @@ from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 
 from clients.authentication.authentication_schema import LoginRequestSchema, RefreshRequestSchema, LoginResponseSchema
+from tools.routes import APIRoutes
+
 
 class AuthenticationClient(APIClient):
     """
@@ -19,7 +21,7 @@ class AuthenticationClient(APIClient):
         :param request: Словарь с email и password.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/authentication/login", json=request.model_dump(by_alias=True))
+        return self.post(f"{APIRoutes.AUTHENTICATION}/login", json=request.model_dump(by_alias=True))
 
     @allure.step("Refresh authentication token")
     def refresh_api(self, request: RefreshRequestSchema) -> Response:
@@ -29,7 +31,7 @@ class AuthenticationClient(APIClient):
         :param request: Словарь с refreshToken.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.post("/api/v1/authentication/refresh", json=request.model_dump(by_alias=True))
+        return self.post(f"{APIRoutes.AUTHENTICATION}/refresh", json=request.model_dump(by_alias=True))
 
 
     def login(self, request: LoginRequestSchema) -> LoginResponseSchema:
